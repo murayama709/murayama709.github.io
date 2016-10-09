4b08c48fdf74ddf6ea795c71bd95202e0b77f493
murayama709 <murayama709@gmail.com>
2016-10-09 22:38:02 +0900

    change to min.js

--- main.js
+++ main.js
@@ -1,28 +1,8 @@
-jQuery(function($){
-  window.odometerOptions = {
-    auto: false
-  };
-
-  odometer = new Odometer({
-    el: $('.roulette')[0],
-    value:0,
-    duration: 5000,
-    animation: 'notcount',
-    theme: 'car',
-    format: '(,ddd).dd',
-  });
-  odometer.render();
- 
-  timerid = setTimeout(function(){
-    odometer.update(100);
-  },5000);
-
-});
 
 function printNum() {
     var max = Number($("#max").val());
     randnum = randInt(max);
-    $(".roulette").text(randnum);
+    $(".odometer").text(randnum);
 }
 
 function randInt(max) {
